2f03b8c5aafb26c41b1dd7a32b5c95771e2f5c2e
Connor <Connorh223@gmail.com>
2021-07-20 17:24:31 -0400

    add index.html

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 console.log(customers)
 
+// Sophia Burns
 const outputDiv = document.getElementById('root')
 const customerCard = document.createElement('div')
 
-customerCard.classList.add('customer1')
+customerCard.classList.add('customer')
+// name
+const firstName = document.createElement('h1')
+firstName.innerText = customers[0].name.first
+const lastName = customers[0].name.last
+firstName.innerText += ' ' + lastName
+customerCard.appendChild(firstName)
+outputDiv.appendChild(customerCard)
+// email
+const email = document.createElement('div')
+email.innerText = customers[0].email
+customerCard.appendChild(email)
+// address
+const streetNumber = document.createElement('dl')
+streetNumber.innerText = customers[0].location.street.number
+customerCard.appendChild(streetNumber)
+const streetName = document.createElement('dl')
+streetName.innerText = customers[0].location.street.name
+customerCard.appendChild(streetName)
+// NEED TO FIX FORMAT^^^
+// address.innerText = customers[0].location.street.name
+// outputDiv.appendChild(customerCard)
+
