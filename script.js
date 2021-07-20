47e1b4669bf1b41e7214e9bcf9a0f389b8097202
Connor <Connorh223@gmail.com>
2021-07-20 18:21:12 -0400

    add script.js

--- script.js
+++ script.js
@@ -26,4 +26,23 @@ customerCard.appendChild(streetName)
 // NEED TO FIX FORMAT^^^
 // address.innerText = customers[0].location.street.name
 // outputDiv.appendChild(customerCard)
-
+// NEW LINE 
+const city = document.createElement('dl')
+city.innerText = customers[0].location.city
+customerCard.appendChild(city)
+const state = document.createElement('dl')
+state.innerText = customers[0].location.state
+customerCard.appendChild(state)
+const postcode = document.createElement('dl')
+postcode.innerText = customers[0].location.postcode
+customerCard.appendChild(postcode)
+// NEW LINE
+// "dob..."
+const date = document.createElement('p')
+date.innerText = customers[0].dob.date
+customerCard.appendChild(date)
+// NEW LINE - FIX THIS SHIT ^
+// "member since..."
+const registered = document.createElement('p')
+registered.innerText = customers[0].registered.date
+customerCard.appendChild(registered)
