5d8be84017022df76f05eaf5293c65eed36eacdb
Connor <Connorh223@gmail.com>
2021-07-20 20:31:12 -0400

    add script.js

--- script.js
+++ script.js
@@ -3,58 +3,77 @@ console.log(customers)
 // Sophia Burns
 const outputDiv = document.getElementById('root')
 const customerCard = document.createElement('div')
+const customer = customers[0]
+for (let customer of customers) { 
+    
 
 customerCard.classList.add('customer')
 
 const picture = document.createElement('img')
-picture.src= customers[0].picture.large
+picture.src = customer.picture.large
 customerCard.appendChild(picture)
-outputDiv.appendChild.picture
+// outputDiv.appendChild.picture
 // name
 const firstName = document.createElement('h1')
-firstName.innerText = customers[0].name.first
-const lastName = customers[0].name.last
+firstName.innerText = customer.name.first
+const lastName = customer.name.last
 firstName.innerText += ' ' + lastName
 customerCard.appendChild(firstName)
-outputDiv.appendChild(customerCard)
+// outputDiv.appendChild(customerCard)
 
 // email
 const email = document.createElement('div')
-email.innerText = customers[0].email
+email.innerText = customer.email
 customerCard.appendChild(email)
 
 // address
 const streetNumber = document.createElement('dl')
-streetNumber.innerText = customers[0].location.street.number
+streetNumber.innerText = customer.location.street.number
 // customerCard.appendChild(streetNumber)
-const streetName = customers[0].location.street.name
+const streetName = customer.location.street.name
 streetNumber.innerText += ' ' + streetName
 customerCard.appendChild(streetNumber)
-outputDiv.appendChild(customerCard)
+// outputDiv.appendChild(customerCard)
 // NEED TO FIX FORMAT^^^
 // (fixed it)
 
 // NEW LINE 
 // city state zip
 const city = document.createElement('dl')
-city.innerText = customers[0].location.city
+city.innerText = customer.location.city
 // customerCard.appendChild(city)
-const state = customers[0].location.state
+const state = customer.location.state
 // city.innerText += ', ' + state
 // customerCard.appendChild(state)
-const postcode = customers[0].location.postcode
+const postcode = customer.location.postcode
 city.innerText += ', ' + state + ' ' + postcode
 customerCard.appendChild(city)
-outputDiv.appendChild(customerCard)
+// outputDiv.appendChild(customerCard)
 
 // NEW LINE
 // "dob..."
 const date = document.createElement('p')
-date.innerText = "DOB; " + moment(customers[0].dob.date). format("MMM DD, YYYY")
+date.innerText = "DOB; " + moment(customer.dob.date). format("MMM DD, YYYY")
 customerCard.appendChild(date)
 // NEW LINE 
 
 // "member since..."
 const registered = document.createElement('p')
-registered.innerText = "Member since " + moment(customers[0].registered.date). format("MMM DD, YYYY")
+registered.innerText = "Member since " + moment(customer.registered.date). format("MMM DD, YYYY")
 customerCard.appendChild(registered)
+
+
+// function FillCustomerCard(customers) {
+//     let (i = 0)
+//     for (i = 0; i < customers.length; i++) {
+//         if (customers[i].name.first = customers.name.first) {
+            
+
+//             outputDiv.appendChild(customerCard)
+//         }
+//     }
+// }
+
+
+
+outputDiv.appendChild(customerCard)}
